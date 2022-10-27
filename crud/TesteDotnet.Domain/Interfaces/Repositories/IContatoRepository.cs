5a27c8ff43ee5fb41dd735b9b05f7573708511d6
arakakivl <arakakivl@proton.me>
2022-10-27 14:26:57 -0300

    Forgot to add new contato repo method in interface :P

--- crud/TesteDotnet.Domain/Interfaces/Repositories/IContatoRepository.cs
+++ crud/TesteDotnet.Domain/Interfaces/Repositories/IContatoRepository.cs
@@ -4,5 +4,5 @@ namespace TesteDotnet.Domain.Interfaces.Repositories;
 
 public interface IContatoRepository : IBaseRepository<Guid, Contato>
 {
-    
+    Task<Contato?> GetContatoAsync(Guid pessoaId, long celular);
 }
