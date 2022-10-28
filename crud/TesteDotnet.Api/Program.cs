cbfe426bdbd48da7936b366c8d9e8ad866f905fc
arakakivl <arakakivl@proton.me>
2022-10-28 19:17:00 -0300

    almost final

--- crud/TesteDotnet.Api/Program.cs
+++ crud/TesteDotnet.Api/Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using TesteDotnet.Application.Services;
+using TesteDotnet.Application.Services.Interfaces;
+using TesteDotnet.Domain.Interfaces;
+using TesteDotnet.Domain.Interfaces.Repositories;
+using TesteDotnet.Infrastructure.Persistence;
+using TesteDotnet.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<IPessoaRepository, PessoaRepository>();
+builder.Services.AddTransient<IContatoRepository, ContatoRepository>();
+builder.Services.AddTransient<IPessoaService, PessoaService>();
+builder.Services.AddTransient<IContatoService, ContatoService>();
 
-builder.Services.AddControllers();
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseInMemoryDatabase("db"));
+
+builder.Services.AddControllers(x => x.SuppressAsyncSuffixInActionNames = false);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,8 +33,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
